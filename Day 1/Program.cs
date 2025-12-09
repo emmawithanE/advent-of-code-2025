6814f2dcb13a3342f861a0801578fb141d675a13
Emma <50310027+emmawithanE@users.noreply.github.com>
2025-12-09 18:39:27 +1030

    Day 1 (A-Side)

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Begin");
+
+int val = 50;
+int zeroes = 0;
+
+foreach (String line in File.ReadLines("input.txt"))
+{
+    char c = line[0];
+    int shift = Int32.Parse(line.Substring(1));
+    switch (c) {
+        case 'L':
+            val -= shift;
+            break;
+        case 'R':
+            val += shift;
+            break;
+        default:
+            Console.WriteLine("unexpected line: " + line);
+            break;
+    }
+
+    val = val % 100;
+
+    if (val == 0) {  zeroes++; }
+
+}
+
+Console.WriteLine("zeroes found: " + zeroes.ToString());
+
+return;
